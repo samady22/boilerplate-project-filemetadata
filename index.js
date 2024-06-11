83be5cd90ccf991a36d4728859be1f920fa5f1f3
Hikmatullah Samady <hikmat.samady1@yahoo.com>
2024-06-11 16:38:33 -0400

    feat: install required packages and add sample.env

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.post("/api/fileanalyse", (req, res) => {
+  console.log("body: ", req.body, "file: ", req.upfile);
+  res.send({ message: "file successfully uploaded!" });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
